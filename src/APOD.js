6188db29d1b776d546f7d1baad2ed7ff88e185a6
user.email <stephanie.enciso13@gmail.com>
2020-12-09 19:00:00 -0600

    I was able to fetch the API data and render the image to the page successfully.

---
+++ src/APOD.js
@@ -0,0 +1,2 @@
+export const imgURL = 'https://api.nasa.gov/planetary/apod';
+export const API_KEY = '?api_key=DEMO_KEY&date=2012-03-14';
--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+import { imgURL, API_KEY } from './APOD';
+
 
 function App() {
+  const [image, setImage] = useState();
+  useEffect(() => {
+    const fetchImage = () => {
+      axios
+        .get(`${imgURL}${API_KEY}`)
+        .then((res) => {
+          setImage(res.data.url)          ;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchImage();
+    
+  }, []);
   return (
     <div className="App">
+      <img src = {image} />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
